928a7ecec66dcc142a9aff2e0431238735dd8384
Sonia <bhatiamandeep94@gmail.com>
2025-04-16 13:30:33 +0100

    cinfigurarions

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const API_BASE_URL = 'http://localhost:8080/api';
+
+export const LOGIN_URL = `${API_BASE_URL}/auth/login`;
+export const REGISTER_URL = `${API_BASE_URL}/auth/register`;
+export const PRODUCTS_URL = `${API_BASE_URL}/products`;
