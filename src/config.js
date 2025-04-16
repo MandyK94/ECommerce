00ac65b230ff6a0de4dffeac9650c17cdf8143d6
Sonia <bhatiamandeep94@gmail.com>
2025-04-16 13:53:09 +0100

    Login component

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const API_BASE_URL = 'http://localhost:8080/api';
 
-export const LOGIN_URL = `${API_BASE_URL}/auth/login`;
-export const REGISTER_URL = `${API_BASE_URL}/auth/register`;
-export const PRODUCTS_URL = `${API_BASE_URL}/products`;
+export const LOGIN_URL = '${API_BASE_URL}/auth/login';
+export const REGISTER_URL = '${API_BASE_URL}/auth/register';
+export const PRODUCTS_URL = '${API_BASE_URL}/products';
+
+export const INVALID_LOGIN_ERROR_MESSAGE = 'Enter valid login id or password. Please try again.';
